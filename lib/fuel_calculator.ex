cc8a17f45507f171c2ed6ce8866fec0685ef73fc
Ajeng Sugiarti <ajeng.sugiarti@gmail.com>
2024-08-25 12:42:20 +0700

    Base fuel calculation

--- lib/fuel_calculator.ex
+++ lib/fuel_calculator.ex
@@ -4,15 +4,46 @@ defmodule FuelCalculator do
   """
 
   @doc """
-  Hello world.
+  Calculate the fuel needed to land on Earth.
 
   ## Examples
 
-      iex> FuelCalculator.hello()
-      :world
+      iex> FuelCalculator.calculate_total_fuel(28801, [{:land, "earth"}])
+      13447
+
+      iex> FuelCalculator.calculate_total_fuel(28801, [{:launch, "earth"}, {:land, "moon"}, {:launch, "moon"}, {:land, "earth"}])
+      51898
 
   """
-  def hello do
-    :world
+  @gravity %{
+    "earth" => 9.807,
+    "moon" => 1.62,
+    "mars" => 3.711
+  }
+
+  def calculate_total_fuel(mass, route) do
+    Enum.reverse(route)
+    |> Enum.reduce(0, fn path, total_fuel_needed ->
+      calculate_needed_fuel(path, mass + total_fuel_needed, total_fuel_needed)
+    end)
+  end
+
+  defp calculate_needed_fuel(_, mass, total_fuel_needed) when mass <= 0, do: total_fuel_needed
+
+  defp calculate_needed_fuel({action, planet} = path, mass, total_fuel_needed) do
+    additional_fuel = calculate_fuel(action, mass, Map.fetch!(@gravity, planet))
+    calculate_needed_fuel(path, additional_fuel, total_fuel_needed + additional_fuel)
+  end
+
+  defp calculate_fuel(:launch, mass, gravity) do
+    (mass * gravity * 0.042 - 33)
+    |> floor()
+    |> max(0)
+  end
+
+  defp calculate_fuel(:land, mass, gravity) do
+    (mass * gravity * 0.033 - 42)
+    |> floor()
+    |> max(0)
   end
 end
--- test/fuel_calculator_test.exs
+++ test/fuel_calculator_test.exs
@@ -2,7 +2,76 @@ defmodule FuelCalculatorTest do
   use ExUnit.Case
   doctest FuelCalculator
 
-  test "greets the world" do
-    assert FuelCalculator.hello() == :world
+  test "calculate the fuel needed to land on Earth" do
+    assert FuelCalculator.calculate_total_fuel(28801, [{:land, "earth"}]) == 13447
+  end
+
+  test "calculate the fuel needed to launch from Moon" do
+    assert FuelCalculator.calculate_total_fuel(28801 + 13447, [{:launch, "moon"}]) == 3001
+  end
+
+  test "calculate the fuel needed to land on Moon" do
+    assert FuelCalculator.calculate_total_fuel(28801 + 13447 + 3001, [{:land, "moon"}]) == 2462
+  end
+
+  test "calculate the fuel needed to launch from Earth" do
+    assert FuelCalculator.calculate_total_fuel(28801 + 13447 + 3001 + 2462, [{:launch, "earth"}]) ==
+             32988
+  end
+
+  test "calculate the fuel needed for journey from Moon to Earth" do
+    assert FuelCalculator.calculate_total_fuel(28801, [
+             {:launch, "moon"},
+             {:land, "earth"}
+           ]) == 16448
+  end
+
+  test "calculate the fuel needed for journey from Earth to Moon and back" do
+    assert FuelCalculator.calculate_total_fuel(28801, [
+             {:launch, "earth"},
+             {:land, "moon"},
+             {:launch, "moon"},
+             {:land, "earth"}
+           ]) == 51898
+  end
+
+  test "calculate the fuel needed for journey from Earth to Mars and back" do
+    assert FuelCalculator.calculate_total_fuel(14606, [
+             {:launch, "earth"},
+             {:land, "mars"},
+             {:launch, "mars"},
+             {:land, "earth"}
+           ]) == 33388
+  end
+
+  test "calculate the fuel needed for journey from Earth to Moon to Mars and back" do
+    assert FuelCalculator.calculate_total_fuel(75432, [
+             {:launch, "earth"},
+             {:land, "moon"},
+             {:launch, "moon"},
+             {:land, "mars"},
+             {:launch, "mars"},
+             {:land, "earth"}
+           ]) == 212_161
+  end
+
+  describe "reverse" do
+    test "calculate the fuel needed to launch from Earth" do
+      assert FuelCalculator.calculate_total_fuel(28801, [{:launch, "earth"}]) == 19772
+    end
+
+    test "calculate the fuel needed to land on Moon" do
+      assert FuelCalculator.calculate_total_fuel(28801 + 19772, [{:land, "moon"}]) == 2648
+    end
+
+    test "calculate the fuel needed to launch from Moon" do
+      assert FuelCalculator.calculate_total_fuel(28801 + 19772 + 2648, [{:launch, "moon"}]) ==
+               3653
+    end
+
+    test "calculate the fuel needed to land on Earth" do
+      assert FuelCalculator.calculate_total_fuel(28801 + 19772 + 2648 + 3653, [{:land, "earth"}]) ==
+               25878
+    end
   end
 end
